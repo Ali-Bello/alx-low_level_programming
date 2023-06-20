394d231a4d0e005a5385dbdd83216d350e7143c6
Bello <m.aliderraj@gmail.com>
2023-06-20 23:16:54 +0100

    Mandatory tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_abs - Gives the absolute value of an integer
+  *@n: the number to work with
+  *
+  *Return: the absolute value
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
