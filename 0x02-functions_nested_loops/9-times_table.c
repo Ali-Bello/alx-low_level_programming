31f2e5ab1906608505d9ae486c6bcf4d635a1963
Bello <m.aliderraj@gmail.com>
2023-06-20 23:51:12 +0100

    Fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,18 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if (k == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
 
-			else if (k < 10)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}
-			else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
