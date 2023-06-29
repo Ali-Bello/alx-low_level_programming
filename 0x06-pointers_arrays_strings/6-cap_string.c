2440b0667129db477cd34b6480af68799f0d1d56
Bello <m.aliderraj@gmail.com>
2023-06-30 00:57:00 +0100

    small error

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,14 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		if (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
+		str[index - 1] == ';' ||
 		    str[index - 1] == '.' ||
 		    str[index - 1] == '!' ||
 		    str[index - 1] == '?' ||
@@ -33,5 +33,5 @@ char *cap_string(char *str)
 
 		index++;
 	}
-
 	return (str);
+}
