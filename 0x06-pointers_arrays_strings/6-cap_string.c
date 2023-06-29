03f0aa4c329855f7321f233c9e8a5929a0728fb8
Bello <m.aliderraj@gmail.com>
2023-06-30 00:58:52 +0100

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,14 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		if (!(str[index] >= 'a' && str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
+		    str[index - 1] == ';' ||
 		    str[index - 1] == '.' ||
 		    str[index - 1] == '!' ||
 		    str[index - 1] == '?' ||
@@ -33,5 +33,6 @@ char *cap_string(char *str)
 
 		index++;
 	}
+
 	return (str);
 }
