8bd6227701977fda2ec2889b75ac77e1668dcbce
Bello <m.aliderraj@gmail.com>
2023-08-31 01:39:16 +0100

    name error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-  * print_rev_recursion - prints a string in reverse;
+  * _print_rev_recursion - prints a string in reverse;
   * @s: string to be printed.
   */
-void	print_rev_recursion(char *s)
+void	_print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		print_rev_recursion(++s);
+		_print_rev_recursion(++s);
 		_putchar(*s);
 	}
 }
