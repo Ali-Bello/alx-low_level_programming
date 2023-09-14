7c32c9d207798f1a8b8597c3600b154d7f6d700d
Bello <m.aliderraj@gmail.com>
2023-09-14 19:14:56 +0100

    assembly

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - program that prints the assembly code of itself.
+  * @ac: argument count.
+  * @av: argument vector.
+  * Return: 0 on succes.
+  */
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		char	*ptr;
+		int	i, bytes;
+
+		i = 0;
+		bytes = atoi(av[1]);
+		if (bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		ptr = (char *)main;
+		while (i < bytes)
+			printf("%02hhx ", ptr[i++]);
+		printf("\n");
+		return (0);
+	}
+	printf("Error\n");
+	exit(1);
+}
