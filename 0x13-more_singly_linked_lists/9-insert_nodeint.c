c14af238d27e3bc9bb38e6b03878f12c8509e5c2
Bello <m.aliderraj@gmail.com>
2023-09-26 23:50:33 +0100

    Edge case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
-	if (*head)
+	if (head)
 	{
 		node = malloc(sizeof(listint_t));
 		if (!node)
@@ -31,6 +31,6 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		tmp->next = node;
 	}
-
+	
 	return (node);
 }
