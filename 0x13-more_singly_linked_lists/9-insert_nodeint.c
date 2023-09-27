bc1f0ebebdd40d762f9ee6a0062f6740eb0b1193
Bello <m.aliderraj@gmail.com>
2023-09-27 01:19:15 +0100

    NULL safety

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,10 +38,7 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	node = malloc(sizeof(listint_t));
 	if (!node)
-	{
-		free(node);
 		return (NULL);
-	}
 
 	node->n = n;
 	node->next = tmp->next;
